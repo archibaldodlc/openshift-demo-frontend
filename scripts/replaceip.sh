1fa93f721269c69f63b890b8f82060f55ffd7de6
Archibaldo De La Cruz <victor.merino@archibaldo.cl>
2021-12-01 02:25:21 -0300

    Fixed typo

--- scripts/replaceip.sh
+++ scripts/replaceip.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 cd ../src/app/services
-sed -i '' 's/API_IP/'${API_URL}'/g' usuario.service.ts
-sed -i '' 's/API_IP/'${API_URL}'/g' parametro.service.ts
+sed -i 's/API_IP/'${API_URL}'/g' usuario.service.ts
+sed -i 's/API_IP/'${API_URL}'/g' parametro.service.ts
